d9e0e40f8b36d95d17afbc8f5bf9ae4b94affd5b
Rashid Mohammed Iliyas Khan <khanr7433@gmail.com>
2025-01-01 17:52:35 +0530

    Add Tailwind CSS configuration, utility functions, and button component; update App component and index styles

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,11 @@
-import React from "react";
+import { Button } from "@/components/ui/Button";
 
 const App = () => {
   return (
     <>
       <div>
-        <h1 className="bg-red-700 text-black">Hello World</h1>
+        <h1 className="text-2xl">Hello</h1>
+        <Button>Click Me</Button>
       </div>
     </>
   );
