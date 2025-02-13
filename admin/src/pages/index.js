3e4fb5f05d3527d26cc5ad9bd76c53ed14a3763d
Rashid Mohammed Iliyas Khan <khanr7433@gmail.com>
2025-02-13 12:37:26 +0530

    fix: correct import path for Login component in admin index file

--- admin/src/pages/index.js
+++ admin/src/pages/index.js
@@ -1,11 +1,11 @@
 import Home from "./Home";
-import Login from "./Login";
 import Register from "./Register";
 import Logout from "./Logout";
 import Profile from "./Profile";
 import ForgotPassword from "./ForgotPassword";
 import ChangePassword from "./ChangePassword";
 import ResetPassword from "./ResetPassword";
+import Login from "./login";
 
 export {
   Home,
